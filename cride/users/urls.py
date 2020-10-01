535b8b62ddf023f38d5201d895514bb3ab3049de
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-30 20:27:15 -0500

    Verify account using pyjwt

--- cride/users/urls.py
+++ cride/users/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 # Views
 from cride.users.views import (
+    AccountVerificationAPIView,
     UserLoginAPIView,
     UserSignUpAPIView
 )
@@ -11,4 +12,5 @@ from cride.users.views import (
 urlpatterns = [
     path('emails/login/', UserLoginAPIView.as_view(), name='login'),
     path('emails/signup/', UserSignUpAPIView.as_view(), name='signup'),
+    path('emails/verify/', AccountVerificationAPIView.as_view(), name='verify'),
 ]
