ac5d6bf9ecdc51ca4551c6df214b769b9de8d637
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-10-02 01:55:47 -0500

    Create circles

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -10,10 +10,17 @@ from cride.circles.models import Circle
 class CircleModelSerializer(serializers.ModelSerializer):
     """Circle model serializer."""
 
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000
+    )
+    is_limited = serializers.BooleanField(default=False)
+
     class Meta:
         """Meta class."""
-        model = Circle
 
+        model = Circle
         fields = (
             'id', 'name', 'slug_name',
             'about', 'picture',
@@ -21,3 +28,11 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'verified', 'is_public',
             'is_limited', 'members_limit'
         )
+
+    def validate(self, data):
+        """Ensure both members_limit and is_limited are present."""
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided')
+        return data
