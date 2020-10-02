425d0e47bfc8a45bb813e2caf1873b440cd2e80b
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-10-02 15:10:56 -0500

    List members

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -7,9 +7,15 @@ from rest_framework.routers import DefaultRouter
 
 # Views
 from .views import circles as circles_views
+from ..circles.views import memberships as membership_views
 
 router = DefaultRouter()
 router.register(r'circles', circles_views.CircleViewSet, basename='circle')
+router.register(
+    r'circles/(?P<slug_name>[-a-zA-Z0-0_]+)/members',
+    membership_views.MembershipViewSet,
+    base_name='membership'
+)
 
 urlpatterns = [
     path('', include(router.urls))
